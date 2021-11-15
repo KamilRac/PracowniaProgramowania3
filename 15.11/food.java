1785e34276974e63e2cb9ebe21567aa1c6e00d89
Kamil Rachwał <79860039+KamilRac@users.noreply.github.com>
2021-11-15 12:07:47 +0100

    Add files via upload

---
+++ 15.11/food.java
@@ -0,0 +1,22 @@
+public class food
+{
+   private String name;
+   private boolean isVege;
+   
+    public String getName(){
+    return name;
+    };
+    
+    public void setName(String newName){
+    this.name=newName;
+    };
+    
+    public boolean getIsVege(){
+    return isVege;
+    };
+    
+    public void setIsVege(boolean newIsVege){
+    this.isVege=newIsVege;
+    }
+    
+}
---
+++ 15.11/water.java
@@ -0,0 +1,32 @@
+public class water
+{
+   private String name;
+   private int mineralAmount;
+   private boolean isCarbonated;
+   
+    public String getName(){
+    return name;
+    };
+    
+    public void setName(String newName){
+    this.name=newName;
+    };
+    
+    public int getMineralAmount(){
+    return mineralAmount;
+    };
+    
+    public void setMineralAmount(int newMineralAmount){
+    this.mineralAmount=newMineralAmount;
+    };
+    
+    
+    public boolean getIsCarbonated(){
+    return isCarbonated;
+    };
+    
+    public void setIsCarbonated(boolean newIsCarbonated){
+    this.isCarbonated=newIsCarbonated;
+    }
+    
+}
